36dcac1c0a2ceaba4ed964d76b241c1e1873b629
roobie001 <meloboy2000@gmail.com>
2025-06-05 16:23:26 +0100

    removed console.logs and uncomment icons

--- src/js/model.js
+++ src/js/model.js
@@ -50,7 +50,6 @@ export const loadSearchResult = async function (query) {
     state.search.query = query;
 
     const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
-    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,6 +1,6 @@
 import View from './view.js';
 import previewView from './previewView.js';
-// import icons from 'url:../../img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
